5f6a7a3e7dd3c86a2f340b7e8301bf2e96fd3eee
soydat <francoalmeida400@gmail.com>
2024-11-06 15:46:36 -0300

    Release update

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
-        resizable:false,
+        resizable:true,
         webPreferences: {
-            contextIsolation: true,
+            contextIsolation: false,
             nodeIntegration: true,
         }
     });
